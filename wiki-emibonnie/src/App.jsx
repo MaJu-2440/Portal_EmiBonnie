a930ea15415a2abffa6e432d303908ed0ae81221
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-11-24 15:38:33 -0300

    importa header mobile

--- wiki-emibonnie/src/App.jsx
+++ wiki-emibonnie/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import Header from './components/Header.jsx'
 
 function App() {
   return (
     <div className="app-container">
+      <Header />
       <h1>Minha Wiki Emibonnie</h1>
       <p>Migração iniciada!</p>
     </div>
---
+++ wiki-emibonnie/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react' // (Opcional nas versões novas, mas bom pra garantir)
+
+function Header() {
+  return (
+    <header>
+        <div className="logo">Portal EmiBonnie</div>
+
+      <nav className="nav-mobile">
+        <div className="nav-icons">
+          <i className="fa-solid fa-bars"></i>
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+
+        <ul className="nav-mobile_lista" hidden>
+            <li className="decoration"><i className="fa-solid fa-caret-up"></i></li>
+          <li>
+            <a href="index.html"
+              >Página Principal<i className="fa-solid fa-angle-right"></i
+            ></a>
+          </li>
+          <li>
+            <a href="#">Wiki Emi<i className="fa-solid fa-angle-right"></i></a>
+          </li>
+          <li>
+            <a href="#">Wiki Bonnie<i className="fa-solid fa-angle-right"></i></a>
+          </li>
+          <li>
+            <a href="us-wiki.html"
+              >Wiki Us<i className="fa-solid fa-angle-right"></i
+            ></a>
+          </li>
+          <li>
+            <a href="#">Galeria<i className="fa-solid fa-angle-right"></i></a>
+          </li>
+          <li>
+            <a href="#">Notícias<i className="fa-solid fa-angle-right"></i></a>
+          </li>
+          <li>
+            <a href="#"
+              >Links/Comunidade<i className="fa-solid fa-angle-right"></i
+            ></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
