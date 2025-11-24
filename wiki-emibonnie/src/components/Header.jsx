485430d1216cc149eb1337302309e515a7e3481c
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-11-24 15:44:21 -0300

    muda logo da header pra ser link clicavel

--- wiki-emibonnie/src/components/Header.jsx
+++ wiki-emibonnie/src/components/Header.jsx
@@ -3,41 +3,43 @@ import React from 'react' // (Opcional nas versões novas, mas bom pra garantir)
 function Header() {
   return (
     <header>
-        <div className="logo">Portal EmiBonnie</div>
+        <div class="logo">
+        <a href="#">Portal EmiBonnie</a>
+    </div>
 
-      <nav className="nav-mobile">
-        <div className="nav-icons">
-          <i className="fa-solid fa-bars"></i>
-          <i className="fa-solid fa-xmark"></i>
+      <nav class="nav-mobile">
+        <div class="nav-icons">
+          <i class="fa-solid fa-bars"></i>
+          <i class="fa-solid fa-xmark"></i>
         </div>
 
-        <ul className="nav-mobile_lista" hidden>
-            <li className="decoration"><i className="fa-solid fa-caret-up"></i></li>
+        <ul class="nav-mobile_lista" hidden>
+            <li class="decoration"><i class="fa-solid fa-caret-up"></i></li>
           <li>
             <a href="index.html"
-              >Página Principal<i className="fa-solid fa-angle-right"></i
+              >Página Principal<i class="fa-solid fa-angle-right"></i
             ></a>
           </li>
           <li>
-            <a href="#">Wiki Emi<i className="fa-solid fa-angle-right"></i></a>
+            <a href="#">Wiki Emi<i class="fa-solid fa-angle-right"></i></a>
           </li>
           <li>
-            <a href="#">Wiki Bonnie<i className="fa-solid fa-angle-right"></i></a>
+            <a href="#">Wiki Bonnie<i class="fa-solid fa-angle-right"></i></a>
           </li>
           <li>
             <a href="us-wiki.html"
-              >Wiki Us<i className="fa-solid fa-angle-right"></i
+              >Wiki Us<i class="fa-solid fa-angle-right"></i
             ></a>
           </li>
           <li>
-            <a href="#">Galeria<i className="fa-solid fa-angle-right"></i></a>
+            <a href="#">Galeria<i class="fa-solid fa-angle-right"></i></a>
           </li>
           <li>
-            <a href="#">Notícias<i className="fa-solid fa-angle-right"></i></a>
+            <a href="#">Notícias<i class="fa-solid fa-angle-right"></i></a>
           </li>
           <li>
             <a href="#"
-              >Links/Comunidade<i className="fa-solid fa-angle-right"></i
+              >Links/Comunidade<i class="fa-solid fa-angle-right"></i
             ></a>
           </li>
         </ul>
