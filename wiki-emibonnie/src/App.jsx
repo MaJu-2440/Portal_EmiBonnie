931c286f92018ae536915ff430b1cbb935726aed
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-11-24 15:12:09 -0300

    instala react por vite

---
+++ wiki-emibonnie/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Minha Wiki Emibonnie</h1>
+      <p>Migração iniciada!</p>
+    </div>
+  )
+}
+
+export default App
