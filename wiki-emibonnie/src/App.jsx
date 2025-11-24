e7507325a392dd1b886b490d882f6e49afc68d19
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-11-24 17:21:45 -0300

    transfere secao de conteudo principal

--- wiki-emibonnie/src/App.jsx
+++ wiki-emibonnie/src/App.jsx
@@ -10,8 +10,68 @@ function App() {
       <Header />
       <main>
         <section class="content_container">
-          <h1>Minha Wiki Emibonnie</h1>
-          <p>Migração iniciada!</p>
+          <section id="principal">
+            <h1>{usData.titulo}</h1>
+
+            <aside className="details_content">
+              <figure className="poster_series">
+                <img src={usData.imagem_capa} alt="Poster da Série Us" />
+              </figure>
+
+              <table className="info_series">
+                <tbody>
+                  <tr>
+                    <th>Elenco</th>
+                    <td>
+                      <a href="#elenco">Veja elenco</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>N° de episódios</th>
+                    <td>{usData.info_tecnica.numero_episodios}</td>
+                  </tr>
+                  <tr>
+                    <th>Duração média</th>
+                    <td>{usData.info_tecnica.duracao_media}</td>
+                  </tr>
+                  <tr>
+                    <th>Exibição</th>
+                    <td>
+                      <a
+                        href="#links"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {usData.info_tecnica.exibicao}
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Primeira exibição</th>
+                    <td>{usData.info_tecnica.primeira_exibicao}</td>
+                  </tr>
+                  <tr>
+                    <th>Última exibição</th>
+                    <td>{usData.info_tecnica.ultima_exibicao}</td>
+                  </tr>
+                  <tr>
+                    <th>Classificação</th>
+                    <td>{usData.info_tecnica.classificacao}</td>
+                  </tr>
+                  <tr>
+                    <th>Língua Original</th>
+                    <td>{usData.info_tecnica.lingua_original}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </aside>
+
+            <p
+              dangerouslySetInnerHTML={{ __html: usData.descricao_completa }}
+            />
+            <p dangerouslySetInnerHTML={{ __html: usData.ficha_tecnica }} />
+            <hr />
+          </section>
 
           <section id="episodios">
             <h2>
