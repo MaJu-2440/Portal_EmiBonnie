8bc0e65dcc399269c36c026a73d6c24cac6c0df1
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-12-16 18:24:47 -0300

    muda logica dos carrosseis para adiconar api swiper

--- wiki-emibonnie/src/components/Carrossel.jsx
+++ wiki-emibonnie/src/components/Carrossel.jsx
@@ -1,58 +1,34 @@
-import { useState, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css"; // estilos básicos
+import "swiper/css/navigation"; // se quiser navegação
 
 export default function Carrossel({ fotos }) {
   if (fotos.length === 0) return null; // Não mostra carrossel vazio
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const carrosselRef = useRef(null);
-
-  const maxScroll = carrosselRef.current
-    ? carrosselRef.current.scrollWidth - carrosselRef.current.clientWidth
-    : 0;
-
-  const scroll = (direction) => {
-    const container = carrosselRef.current;
-    const scrollAmount = 300;
-    const newPosition =
-      scrollPosition + (direction === "left" ? -scrollAmount : scrollAmount);
-
-    if (newPosition < 0) {
-      setScrollPosition(0);
-      container.scrollLeft = 0;
-    } else if (newPosition > maxScroll) {
-      setScrollPosition(maxScroll);
-      container.scrollLeft = maxScroll;
-    } else {
-      container.scrollLeft = newPosition;
-      setScrollPosition(newPosition);
-    }
-
-    container.style.scrollBehavior = "smooth";
-  };
 
   return (
-    <div className="carrossel-container">
-      <button
-        className="carrossel-btn carrossel-btn-left"
-        onClick={() => scroll("left")}
-      >
-        &lt;
-      </button>
-      <div className="carrossel-scroll" ref={carrosselRef}>
-        {fotos.map((foto) => (
+    <Swiper
+      modules={[Autoplay, Pagination, Navigation]}
+      spaceBetween={10}
+      slidesPerView={1} // mobile-first: 1 slide por vez
+      navigation={{ clickable: true }}
+      autoplay={{
+        delay: 10000, // tempo entre slides (ms)
+        disableOnInteraction: false, // continua mesmo após interação
+      }}
+      style={{ width: "100%" }}
+    >
+      {fotos.map((foto) => (
+        <SwiperSlide>
           <div key={foto.id} className="carrossel-item">
             {/* O segredo: Altura fixa, largura automática */}
-            <img src={foto.url} alt={foto.legenda} />
+
+            <img src={foto.url} className="img" alt={foto.legenda} />
             <p className="legenda">{foto.legenda}</p>
             <span className="creditos">{foto.creditos}</span>
           </div>
-        ))}
-      </div>
-      <button
-        className="carrossel-btn carrossel-btn-right"
-        onClick={() => scroll("right")}
-      >
-        &gt;
-      </button>
-    </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
