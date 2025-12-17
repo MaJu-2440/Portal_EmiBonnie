11b349fa842feda9b8693142ea81f98c5760c4ca
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-12-17 16:11:20 -0300

    ajusta links e menus da nav da header

--- wiki-emibonnie/src/components/Header.jsx
+++ wiki-emibonnie/src/components/Header.jsx
@@ -10,6 +10,13 @@ function Header() {
     setIsOpen(!isOpen);
   };
 
+  const [openSubMenu, setOpenSubMenu] = useState(null); // guarda o submenu aberto
+
+  const handleSubMenuToggle = (id) => {
+    // se já estiver aberto, fecha; senão abre o novo
+    setOpenSubMenu(openSubMenu === id ? null : id);
+  };
+
   return (
     <header>
       <div className="logo">
@@ -18,13 +25,13 @@ function Header() {
         </Link>
       </div>
 
-      <nav onClick={handleMenuToggle} className="nav-mobile">
+      <nav className="nav-mobile">
         {isOpen ? (
-          <div className="nav-icons toggle">
+          <div className="nav-icons toggle" onClick={handleMenuToggle}>
             <i className="fa-solid fa-xmark"></i>
           </div>
         ) : (
-          <div className="nav-icons">
+          <div className="nav-icons" onClick={handleMenuToggle}>
             <i className="fa-solid fa-bars"></i>
           </div>
         )}
@@ -34,51 +41,102 @@ function Header() {
             <i className="fa-solid fa-caret-up"></i>
           </li>
           <li>
-            <Link to={"/"} onClick={handleMenuToggle}>
+            <Link to={"/"} onClick={handleMenuToggle} className="link">
               Página Principal<i className="fa-solid fa-angle-right"></i>
             </Link>
           </li>
-          <li>
-            <Link to={"wiki/emi"} onClick={handleMenuToggle}>
-              Wiki Emi Thasorn<i className="fa-solid fa-angle-right"></i>
-            </Link>
+
+          <li onClick={() => handleSubMenuToggle("menu1")} className="link">
+            Perfil<i className="fa-solid fa-angle-right"></i>
           </li>
-          <li>
-            <Link to={"wiki/bonnie"} onClick={handleMenuToggle}>
-              Wiki Bonnie Pattraphus<i className="fa-solid fa-angle-right"></i>
-            </Link>
+          {openSubMenu === "menu1" && (
+            <ul className="nav-mobile_sub-lista">
+              <li>
+                <Link
+                  to={"perfil/emi"}
+                  className="link"
+                  onClick={handleMenuToggle}
+                >
+                  Emi<i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"perfil/bonnie"}
+                  className="link"
+                  onClick={handleMenuToggle}
+                >
+                  Bonnie<i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </li>
+            </ul>
+          )}
+
+          <li onClick={() => handleSubMenuToggle("menu2")} className="link">
+            Wiki<i className="fa-solid fa-angle-right"></i>
           </li>
-          <li>
-            <Link to={"wiki/us-series"} onClick={handleMenuToggle}>
-              Wiki Us the series<i className="fa-solid fa-angle-right"></i>
-            </Link>
+          {openSubMenu === "menu2" && (
+            <ul className="nav-mobile_sub-lista">
+              <li>
+                <Link
+                  to={"wiki/us-series"}
+                  onClick={handleMenuToggle}
+                  className="link"
+                >
+                  Us the series<i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"wiki/moonshadow-series"}
+                  onClick={handleMenuToggle}
+                  className="link"
+                >
+                  Moonshadow<i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </li>
+            </ul>
+          )}
+
+          <li onClick={() => handleSubMenuToggle("menu3")} className="link">
+            Trabalhos<i className="fa-solid fa-angle-right"></i>
           </li>
+          {openSubMenu === "menu3" && (
+            <ul className="nav-mobile_sub-lista">
+              <li>
+                <Link
+                  to={"trabalhos/discografia"}
+                  onClick={handleMenuToggle}
+                  className="link"
+                >
+                  Discografia<i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"trabalhos/filmografia"}
+                  onClick={handleMenuToggle}
+                  className="link"
+                >
+                  Filmografia<i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"trabalhos/entrevistas-e-sessoes-de-fotos"}
+                  onClick={handleMenuToggle}
+                  className="link"
+                >
+                  Entrevistas/Photoshoots
+                  <i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </li>
+            </ul>
+          )}
+
           <li>
-            <Link to={"wiki/moonshadow-series"} onClick={handleMenuToggle}>
-              Wiki Moonshadow<i className="fa-solid fa-angle-right"></i>
-            </Link>
-          </li>
-          <li>
-            <Link to={"galeria"} onClick={handleMenuToggle}>
+            <Link to={"galeria"} onClick={handleMenuToggle} className="link">
               Galeria<i className="fa-solid fa-angle-right"></i>
-            </Link>
-          </li>
-          <li>
-            <Link to={"trabalhos/discografia"} onClick={handleMenuToggle}>
-              Discografia<i className="fa-solid fa-angle-right"></i>
-            </Link>
-          </li>
-          <li>
-            <Link to={"trabalhos/filmografia"} onClick={handleMenuToggle}>
-              Filmografia<i className="fa-solid fa-angle-right"></i>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={"trabalhos/entrevistas-e-sessoes-de-fotos"}
-              onClick={handleMenuToggle}
-            >
-              Entrevistas/Photoshoots<i className="fa-solid fa-angle-right"></i>
             </Link>
           </li>
         </ul>
