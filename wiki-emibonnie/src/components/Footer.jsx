0e129a7d3b8d67b8874fb87986638dfa9fcd672c
Maria Julia <mariajuliapacheco2020@gmail.com>
2025-12-19 20:32:50 -0300

    ajusta cursor e hover do indice desktop

--- wiki-emibonnie/src/components/Footer.jsx
+++ wiki-emibonnie/src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <p>Wiki EmiBonnie - Desenvolvido por MaJu &copy; 2025</p>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <p>Portal EmiBonnie - Desenvolvido por MaJu Pacheco &copy; 2025</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
